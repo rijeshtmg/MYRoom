47faee00ef219af00fcb7024806cbad2038c680d
anish-aryal <anisharyal058@gmail.com>
2023-03-12 00:44:05 +0545

    Login page complete

--- 03 [FYP TOPIC CODE] - Development/Development/client/src/pages/Register.js
+++ 03 [FYP TOPIC CODE] - Development/Development/client/src/pages/Register.js
@@ -1,54 +1,85 @@
-import {useState} from "react";
+// Register.js
+import { useState } from "react";
+import axios from "axios";
+
+
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
 
 export default function Register() {
+  // state
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  //hooks 
+  const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try{
-            console.table({email,password});
-        }
-        catch(err){
-            console.log(err);
-        }
-    };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+   try {
+      console.log(email, password);
+      setLoading(true);
+      const {data} = await axios.post(`/pre-register`, {
+        email,
+        password,
+      },);
+      if(data?.error){
+        toast.error(data.error);
+        setLoading(false);
+      }
+      else{
+        toast.success("Please check your email");
+        setLoading(false);
+        navigate("/");
+      }
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+      toast.error("Something went wrong. Try again.");
+      setLoading(false);
+    
+    }
+  };
 
   return (
-    <div>
-      <h1 className="display-1 bg-primary text-light p-5">Register</h1>
+    // <div>
+    //   <h1 className="display-1 bg-primary text-light p-5">Register</h1>
 
-      <div className="container">
-        <div className="row">
-          <div className="col-md-4 offset-md-4 mt-5">
-            <form onSubmit={handleSubmit}>
-              <input
-                type="text"
-                placeholder="Enter your email"
-                className="form-control mb-4"
-                required
-                autoFocus
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
+    //   <div className="container">
+    //     <div className="row">
+    //       <div className="col-md-4 offset-md-4 mt-5">
+    //         <form onSubmit={handleSubmit}>
+    //           <input
+    //             type="email"
+    //             placeholder="Enter your email"
+    //             className="form-control mb-4"
+    //             value={email}
+    //             onChange={(e) => setEmail(e.target.value.toLowerCase())}
+    //             required
+    //             autoFocus
+    //           />
 
-              <input
-                type="password"
-                placeholder="Enter your password"
-                className="form-control mb-4"
-                required
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
+    //           <input
+    //             type="password"
+    //             placeholder="Enter your password"
+    //             className="form-control mb-4"
+    //             value={password}
+    //             onChange={(e) => setPassword(e.target.value)}
+    //             required
+    //           />
 
-              <button className="btn btn-primary col-12 mb-4">Register</button>
-            </form>
+    //           <button disabled={loading} className="btn btn-primary col-12 mb-4">{loading ? "Waiting.." : "Register"}</button>
+    //         </form>
 
-            {/* <a className="text-danger pointer">Forgot Password</a> */}
-          </div>
-        </div>
-      </div>
-    </div>
+    //         <a className="text-danger pointer" href="/">Forgot Password</a>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+
+  
   );
 }
